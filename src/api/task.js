37ac5b8ed6b04675d0539b4698efc72f74d6acfd
matheusmartinsviana <matheusmviana@outlook.com.br>
2024-06-23 12:48:36 -0300

    feat: view api done

---
+++ src/api/task.js
@@ -0,0 +1,48 @@
+const TaskController = require('../controllers/task')
+
+class TaskApi {
+    async createTask(req, res) {
+        const { title, description, projectId } = req.body
+
+        try {
+            const task = await TaskController.create(title, description, projectId)
+            return res.status(201).send(task)
+        } catch (e) {
+            return res.status(400).send({ error: `Error creating task: ${e.message}` })
+        }
+    }
+
+    async updateTask(req, res) {
+        const { id } = req.params
+        const { title, description, status, conclusionDate, projectId } = req.body
+
+        try {
+            const task = TaskController.update(Number(id), title, description, status, conclusionDate, projectId)
+            return res.status(200).send(task)
+        } catch (e) {
+            return res.status(400).send({ error: `Error updating task: ${e.message}`})
+        }
+    }
+
+    async deleteTask(req, res){
+        const { id } = req.params
+
+        try {
+            await TaskController.delete(Number(id))
+            return res.status(204).send()
+        } catch (e) {
+            return res.status(400).send({ error: `Error deleting task: ${e.message}`})
+        }
+    }
+
+    async findTasks(req, res) {
+        try {
+            const task = await TaskController.find()
+            return res.status(200).send(task)
+        } catch (e) {
+            return res.status(400).send({ error: `Error listing tasks: ${e.message}`})
+        }
+    }
+}
+
+module.exports = new TaskApi()
