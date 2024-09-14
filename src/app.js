ad6f5fe470ab4b1394b53a27091f26fcbeac6ffb
matheusmartinsviana <matheusmviana@outlook.com.br>
2024-09-14 12:51:50 -0300

    chore: add cors config

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cookie_parser());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
 module.exports = app;
